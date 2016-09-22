bf493690efbd0758d4ae47dc28ec88ba82dc0f38
Shifa Khan <lazyfish@MacBook-Air.local>
2016-09-22 21:44:58 +0530

    Routing, styling

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,34 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }          from './app.component';
+import { HeroDetailComponent }   from './hero-detail.component';
+import { HeroesComponent }       from './heroes.component'
+import { DashboardComponent }    from './dashboard.component'
+import { HeroService }           from './hero.service';
+
+import { AppRoutingModule }      from './app-routing.module';
+
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ 
+    AppComponent
+  ]
 })
 export class AppModule { }
--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -10,35 +10,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
-var common_1 = require('@angular/common');
 var hero_service_1 = require('./hero.service');
-var HeroDetailComponent = (function () {
-    function HeroDetailComponent(heroService, route, location) {
+var DashboardComponent = (function () {
+    function DashboardComponent(heroService, router) {
         this.heroService = heroService;
-        this.route = route;
-        this.location = location;
+        this.router = router;
+        this.heroes = [];
     }
-    HeroDetailComponent.prototype.ngOnInit = function () {
+    DashboardComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.route.params.forEach(function (params) {
-            var id = +params['id'];
-            _this.heroService.getHero(id)
-                .then(function (hero) { return _this.hero = hero; });
-        });
+        this.heroService.getHeroes()
+            .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
     };
-    HeroDetailComponent.prototype.goBack = function () {
-        this.location.back();
+    DashboardComponent.prototype.gotoDetail = function (hero) {
+        var link = ['/detail', hero.id];
+        this.router.navigate(link);
     };
-    HeroDetailComponent = __decorate([
+    DashboardComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
-            selector: 'my-hero-detail',
-            templateUrl: 'hero-detail.component.html',
-            styleUrls: ['hero-detail.component.css']
+            selector: 'my-dashboard',
+            templateUrl: 'dashboard.component.html',
+            styleUrls: ['dashboard.component.css']
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.ActivatedRoute, common_1.Location])
-    ], HeroDetailComponent);
-    return HeroDetailComponent;
+        __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.Router])
+    ], DashboardComponent);
+    return DashboardComponent;
 }());
-exports.HeroDetailComponent = HeroDetailComponent;
-//# sourceMappingURL=hero-detail.component.js.map
+exports.DashboardComponent = DashboardComponent;
+//# sourceMappingURL=dashboard.component.js.map
